6dce12a80ddbe64d50c610f90c225dc1bd896029
laurent naitza <lolonaitza@gmail.com>
2020-05-24 21:54:09 +0200

    add dotenv config OP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-
 import * as serviceWorker from "./serviceWorker";
+require("dotenv").config();
 
 ReactDOM.render(
   <React.StrictMode>
