103037c086d1d9de4bbcb6cbc0551270426a26ad
laurent naitza <lolonaitza@gmail.com>
2020-07-09 01:04:16 +0200

    services pages style

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -8,8 +8,65 @@ export default function Services() {
   }, []);
   return (
     <div className="services-container">
-      <h6>Besoin d'information, d'un devis ou même d'une pomme de pin ?</h6>
-      <ServicesCards />
+      <div className="services-header">
+        <div className="services-presentation">
+          <h5>
+            Besoin d’un site vitrine ou e-Commerce ? <br /> d'un modèle, du
+            sur-mesure ?
+          </h5>
+          <div className="services-items">
+            <div className="services-item-title">
+              <h6>Site vitrine</h6>
+              <p>
+                Idéal pour présenter votre activité et trouver de nouveaux
+                clients via le Web.
+              </p>
+            </div>
+            <div className="services-item-desc">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <div className="services-items">
+            <div className="services-item-title">
+              <h6>e-commerce</h6>
+              <p>
+                Le site idéal pour faire de la vente en ligne et booster votre
+                entreprise.
+              </p>
+            </div>
+            <div className="services-item-desc">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <div className="services-items">
+            <div className="services-item-title">
+              <h6>Administratif ou Sur-mesure</h6>
+              <p>
+                Gérez votre site web comme votre boutique ou votre entreprise.
+                Vous modifiez et gérez vos contenus à votre guise. Un projet
+                plus complexe ? Une solution sur-mesure pour répondre exactement
+                à vos attentes. Nous développons pour vous un outil à votre
+                image.
+              </p>
+            </div>
+            <div className="services-item-desc">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <div className="services-items">
+            <div className="services-item-title">
+              <h6>Impression</h6>
+              <p>
+                Fiers de nos 15 ans d'expérience, nous gérons vos impressions et
+                vous orientons vers les meilleurs choix
+              </p>
+            </div>
+            <div className="services-item-desc">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
