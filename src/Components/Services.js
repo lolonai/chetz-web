1047a9daf21f0c9a82b1c7b4c8422d82b39f72e4
laurent naitza <lolonaitza@gmail.com>
2020-07-08 02:02:29 +0200

    page services style

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ServicesCards from "../Containers/Services_cards";
+import AnimationScroll from "../Custom/scrollReveal";
 
 export default function Services() {
+  useEffect(() => {
+    AnimationScroll();
+  }, []);
   return (
     <div className="services-container">
       <h6>Besoin d'information, d'un devis ou même d'une pomme de pin ?</h6>
--- src/Containers/Services_cards.js
+++ src/Containers/Services_cards.js
@@ -1,56 +1,71 @@
 import React from "react";
+import { NavLink, withRouter } from "react-router-dom";
 
-export default function ServicesCards() {
+function ServicesCards() {
   return (
     <div className="cards-container">
       <div className="services-cards">
-        <div className="services-card slide-up-interval load-hidden">
-          <div className="services-card-title">
-            <h6>Site vitrine</h6>
+        <NavLink exact to="/services">
+          <div className="services-card slide-up-interval load-hidden">
+            <div className="services-card-title">
+              <i class="fas fa-glass-martini-alt"></i>
+              <h6>Site vitrine</h6>
+            </div>
+            <div className="services-card-content">
+              <p>
+                Une opportunitée de présenter votre activité sur la toile. Nous
+                réalisons l’intégralité des fonctionnalités pour vous !
+              </p>
+            </div>
           </div>
-          <div className="services-card-content">
-            <p>
-              Une opportunitée de présenter votre activité sur la toile. Nous
-              réalisons l’intégralité des fonctionnalités pour vous !
-            </p>
+        </NavLink>
+        <NavLink exact to="/services">
+          <div className="services-card slide-up-interval load-hidden">
+            <div className="services-card-title">
+              <i class="fas fa-store"></i>
+              <h6>Création e-commerce</h6>
+            </div>
+            <div className="services-card-content">
+              <p>
+                Création clés en mains de votre site E-commerce, conseil,
+                paiement, livraison... Nous développons avec vous votre
+                plateforme de vente en ligne.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="services-card slide-up-interval load-hidden">
-          <div className="services-card-title">
-            <h6>Création e-commerce</h6>
+        </NavLink>
+        <NavLink exact to="/services">
+          <div className="services-card slide-up-interval load-hidden">
+            <div className="services-card-title">
+              <i class="fas fa-drafting-compass"></i>
+              <h6>Application sur-mesure</h6>
+            </div>
+            <div className="services-card-content">
+              <p>
+                Un projet plus complexe ? Une solution sur-mesure pour répondre
+                exactement à vos attentes. Nous développons pour vous un outil à
+                votre image.
+              </p>
+            </div>
           </div>
-          <div className="services-card-content">
-            <p>
-              Création clés en mains de votre site E-commerce, conseil,
-              paiement, livraison... Nous développons avec vous votre plateforme
-              de vente en ligne.
-            </p>
+        </NavLink>
+        <NavLink exact to="/services">
+          <div className="services-card slide-up-interval load-hidden">
+            <div className="services-card-title">
+              <i class="fas fa-paper-plane"></i>
+              <h6>Impression</h6>
+            </div>
+            <div className="services-card-content">
+              <p>
+                Fiers de nos 15 ans d'expérience, nous gérons vos impressions et
+                vous orientons vers les meilleurs choix
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="services-card slide-up-interval load-hidden">
-          <div className="services-card-title">
-            <h6>Application sur-mesure</h6>
-          </div>
-          <div className="services-card-content">
-            <p>
-              Un projet plus complexe ? Une solution sur-mesure pour répondre
-              exactement à vos attentes. Nous développons pour vous un outil à
-              votre image.
-            </p>
-          </div>
-        </div>
-        <div className="services-card slide-up-interval load-hidden">
-          <div className="services-card-title">
-            <h6>Impression</h6>
-          </div>
-          <div className="services-card-content">
-            <p>
-              Fiers de nos 15 ans d'expérience, nous gérons vos impressions et
-              vous orientons vers les meilleurs choix
-            </p>
-          </div>
-        </div>
+        </NavLink>
       </div>
     </div>
   );
 }
+
+export default withRouter(ServicesCards);
